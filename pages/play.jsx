dbcf4af00100086a2de7e80ddbb737a6fc14760d
Scott Mitchell <scott.a.mitchell1@hotmail.com>
2022-10-28 21:55:24 +0800

    revert back to previous backend

--- pages/play.jsx
+++ pages/play.jsx
@@ -17,7 +17,9 @@ export default function Home() {
 
   const getWorlde = async () => {
     try {
-      const res = await fetch(`https://wordlemaker.glitch.me/${wordleId}`);
+      const res = await fetch(
+        `https://custom-wordle-backend.onrender.com/${wordleId}`
+      );
       const data = await res.json();
       setSolution(data.word);
       setMessage(null);
